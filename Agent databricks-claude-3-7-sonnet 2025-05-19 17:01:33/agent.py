2353d60ccd4bc8ea49d630ae3a7630428163aeaf
nova-unknown <nova-unknown>
2025-05-22 15:13:52 +0000

    agent folder added

---
+++ Agent databricks-claude-3-7-sonnet 2025-05-19 17:01:33/agent.py
@@ -0,0 +1,186 @@
+from typing import Any, Generator, Optional, Sequence, Union
+
+import mlflow
+from databricks_langchain import (
+    ChatDatabricks,
+    VectorSearchRetrieverTool,
+    DatabricksFunctionClient,
+    UCFunctionToolkit,
+    set_uc_function_client,
+)
+from langchain_core.language_models import LanguageModelLike
+from langchain_core.runnables import RunnableConfig, RunnableLambda
+from langchain_core.tools import BaseTool
+from langgraph.graph import END, StateGraph
+from langgraph.graph.graph import CompiledGraph
+from langgraph.graph.state import CompiledStateGraph
+from langgraph.prebuilt.tool_node import ToolNode
+from mlflow.langchain.chat_agent_langgraph import ChatAgentState, ChatAgentToolNode
+from mlflow.pyfunc import ChatAgent
+from mlflow.types.agent import (
+    ChatAgentChunk,
+    ChatAgentMessage,
+    ChatAgentResponse,
+    ChatContext,
+)
+
+mlflow.langchain.autolog()
+
+client = DatabricksFunctionClient()
+set_uc_function_client(client)
+
+############################################
+# Define your LLM endpoint and system prompt
+############################################
+LLM_ENDPOINT_NAME = "databricks-claude-3-7-sonnet"
+llm = ChatDatabricks(endpoint=LLM_ENDPOINT_NAME)
+
+system_prompt = """You are a friendly and professional customer service agent for a car service center.
+ 
+Always greet the customer politely when the conversation begins. Then ask for their mobile number if it hasn't been provided yet.
+ 
+Once you have the mobile number:
+ 
+1. Check if the customer exists
+   - If the customer exists:
+     - Retrieve their details and associated vehicles.
+     - Greet them by name.
+     - Ask how you can help today (e.g., book a service, modify booking, cancel, register a new car, check service history).
+   - If the customer does NOT exist:
+     - Let them know you’ll help get them registered.
+     - Ask for their full name, phone number, and email.
+     - Insert their details into the `customer` table.
+     - Then ask about their car (model, plate number, year) 
+ 
+After registration or customer verification, continue the conversation to understand the purpose of the call and help them accordingly:
+- Book an appointment
+- Modify or cancel an existing booking
+- Register a new vehicle
+- Check service history
+Always be courteous, ask one question at a time, and confirm key actions."""
+
+###############################################################################
+## Define tools for your agent, enabling it to retrieve data or take actions
+## beyond text generation
+## To create and see usage examples of more tools, see
+## https://learn.microsoft.com/azure/databricks/generative-ai/agent-framework/agent-tool
+###############################################################################
+tools = []
+
+# You can use UDFs in Unity Catalog as agent tools
+uc_tool_names = ["datalink.lineagedemo.*"]
+uc_toolkit = UCFunctionToolkit(function_names=uc_tool_names)
+tools.extend(uc_toolkit.tools)
+
+
+# # (Optional) Use Databricks vector search indexes as tools
+# # See https://learn.microsoft.com/azure/databricks/generative-ai/agent-framework/unstructured-retrieval-tools
+# # for details
+#
+# # TODO: Add vector search indexes as tools or delete this block
+# vector_search_tools = [
+#         VectorSearchRetrieverTool(
+#         index_name="",
+#         # filters="..."
+#     )
+# ]
+# tools.extend(vector_search_tools)
+
+
+#####################
+## Define agent logic
+#####################
+
+
+def create_tool_calling_agent(
+    model: LanguageModelLike,
+    tools: Union[Sequence[BaseTool], ToolNode],
+    system_prompt: Optional[str] = None,
+) -> CompiledGraph:
+    model = model.bind_tools(tools)
+
+    # Define the function that determines which node to go to
+    def should_continue(state: ChatAgentState):
+        messages = state["messages"]
+        last_message = messages[-1]
+        # If there are function calls, continue. else, end
+        if last_message.get("tool_calls"):
+            return "continue"
+        else:
+            return "end"
+
+    if system_prompt:
+        preprocessor = RunnableLambda(
+            lambda state: [{"role": "system", "content": system_prompt}]
+            + state["messages"]
+        )
+    else:
+        preprocessor = RunnableLambda(lambda state: state["messages"])
+    model_runnable = preprocessor | model
+
+    def call_model(
+        state: ChatAgentState,
+        config: RunnableConfig,
+    ):
+        response = model_runnable.invoke(state, config)
+
+        return {"messages": [response]}
+
+    workflow = StateGraph(ChatAgentState)
+
+    workflow.add_node("agent", RunnableLambda(call_model))
+    workflow.add_node("tools", ChatAgentToolNode(tools))
+
+    workflow.set_entry_point("agent")
+    workflow.add_conditional_edges(
+        "agent",
+        should_continue,
+        {
+            "continue": "tools",
+            "end": END,
+        },
+    )
+    workflow.add_edge("tools", "agent")
+
+    return workflow.compile()
+
+
+class LangGraphChatAgent(ChatAgent):
+    def __init__(self, agent: CompiledStateGraph):
+        self.agent = agent
+
+    def predict(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> ChatAgentResponse:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+
+        messages = []
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                messages.extend(
+                    ChatAgentMessage(**msg) for msg in node_data.get("messages", [])
+                )
+        return ChatAgentResponse(messages=messages)
+
+    def predict_stream(
+        self,
+        messages: list[ChatAgentMessage],
+        context: Optional[ChatContext] = None,
+        custom_inputs: Optional[dict[str, Any]] = None,
+    ) -> Generator[ChatAgentChunk, None, None]:
+        request = {"messages": self._convert_messages_to_dict(messages)}
+        for event in self.agent.stream(request, stream_mode="updates"):
+            for node_data in event.values():
+                yield from (
+                    ChatAgentChunk(**{"delta": msg}) for msg in node_data["messages"]
+                )
+
+
+# Create the agent object, and specify it as the agent object to use when
+# loading the agent back for inference via mlflow.models.set_model()
+agent = create_tool_calling_agent(llm, tools, system_prompt)
+AGENT = LangGraphChatAgent(agent)
+mlflow.models.set_model(AGENT)
